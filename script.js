bd80deb75a4ef8f228bc9b385aa0fbd035adfd75
RussellRothenbach <russroth23@gmail.com>
2020-05-06 16:44:15 +0000

    match class created

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ function flipCard() {
     this.classList.toggle('flip');
 }
 
+class Match {
+    constructor(totalTime, cards) {
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById(timeRemaining)
+        this.cardsArray = cards;
+    }
+    
+}
+
 cards.forEach(card => card.addEventListener('click', flipCard));
