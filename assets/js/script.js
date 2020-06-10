3b30b1e6545b95ddecc6c72e5e1d5c24f724cea2
RussellRothenbach <russroth23@gmail.com>
2020-06-10 18:15:28 +0000

    cleaned up some code, validated  and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 //variables
+//variables for the game 
 const cards = document.querySelectorAll('.card');
-
 let cardFlipped = false;
-let lockBoard = false;
+let boardLock = false;
 let firstCard, secondCard;
+
+//variables for the flip counter
 let flipCount =0;
 let ticker = document.getElementById('flips');
 
-let overlays = Array.from(document.getElementsByClassName('end-modal'));
+//variable for the overlay that appears when the page is loaded or refreshed from https://www.freecodecamp.org/forum/t/adding-class-to-multiple-elements-same-class-javascript/211922
+let overlays = Array.from(document.getElementsByClassName('overlay'));
 overlays.forEach(overlay => {
     overlay.addEventListener('click', () => {
         overlay.classList.remove('visible');
@@ -16,8 +19,9 @@ overlays.forEach(overlay => {
 
 //functions
 
-function flipCard() {
-    if (lockBoard) return;
+//card flip that also adds to the flip counter everytime a card is flipped
+function cardFlip() {
+    if (boardLock) return;
     if (this ===firstCard) return;
     flipCount ++;
     ticker.innerText = flipCount;
@@ -28,38 +32,41 @@ function flipCard() {
         firstCard = this;
         return;
     }    
-        
     cardFlipped = false;
-     secondCard = this;
-
-    checkForMatch();
+    secondCard = this;
+    checkIfMatch();
 }
 
-function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
+//checks whether the dataset.img for the two currently flipped matches
+function checkIfMatch() {
+    let isMatch = firstCard.dataset.img === secondCard.dataset.img;
+    isMatch ? disableCards() : cardUnFlip();
 }
 
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-    resetBoard();
-}
-
-function unflipCards() {
-    lockBoard = true;
+//if no match, the cards will unflip to their original position
+function cardUnFlip() {
+    boardLock = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-        resetBoard();
+        resetGame();
     }, 1500);
 }
 
-function resetBoard() {
-    [cardFlipped, lockBoard] = [false, false];
+//disables cards after two have been flipped so three cards cannot click in quick succession
+function disableCards() {
+    firstCard.removeEventListener('click', cardFlip);
+    secondCard.removeEventListener('click', cardFlip);
+    resetGame();
+}
+
+
+function resetGame() {
+    [cardFlipped, boardLock] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
+//shuffle function from https://stackoverflow.com/questions/58071245/queryselectorall-wont-work-for-shuffling-the-order-of-that-list
 (function shuffle() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 20);
@@ -67,4 +74,4 @@ function resetBoard() {
     });
 })();
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.addEventListener('click', cardFlip));
